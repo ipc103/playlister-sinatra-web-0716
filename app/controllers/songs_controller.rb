5334e95a0e364b8c07aaee25cadeb918722ca4ab
ipc103 <iancandysemail@gmail.com>
2016-08-16 17:00:17 -0400

    added challenge

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,26 @@
+class SongsController < ApplicationController
+
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
+  get '/songs/:id' do
+    @song = Song.find(params[:id])
+    erb :'songs/show'
+  end
+
+  post '/songs' do
+    # {song: {name: 'Kenny G Christmas', genre_ids: [1, 2, 3]}}
+    @song = Song.new(params[:song])
+
+    params[:song][:genre_ids].each do |genre_id|
+      @song.genres << Genre.find(genre_id)
+    end
+
+    @song.save
+
+    redirect "/songs/#{@song.id}"
+  end
+
+end
